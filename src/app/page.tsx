19e17da24be31edea8a8aa226c28821cf6b630df
iUnstable0 <me@iunstable0.com>
2025-12-20 21:47:23 -0500

    sliding number component and basic level slection design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,40 @@
+"use client";
+
+import React, { useState } from "react";
+
 import Image from "next/image";
 
 import styles from "./page.module.scss";
 
+import { SlidingNumber } from "@/components/mp/sliding-number";
+
 import levels from "@/components/levels.json";
 
 export default function Home() {
+  const [selectedLevel, setSelectedLevel] = useState<number>(1);
+
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.header}>
           <h1 className={styles.title}>welcome to kapow</h1>
 
-          <Image
-            src={"/1.jpg"}
-            alt={"food"}
-            width={400}
-            height={400}
-            className={styles.kapow}
-          />
+          <div className={styles.levels}>
+            <div className={styles.controls}>
+              <button className={styles.select}>{"<"}</button>
+              <button className={styles.select}>{">"}</button>
+            </div>
+            <Image
+              src={`/${selectedLevel}.jpg`}
+              alt={"food"}
+              width={400}
+              height={400}
+              className={styles.kapow}
+            />
+          </div>
         </div>
+
+        <SlidingNumber value={selectedLevel} />
 
         {/*<div>*/}
         {/*  <h1>Choose your level:</h1>*/}
